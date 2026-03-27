163d621a0f70221b33ef736786af01027e9a74ff
Coizvixxy <iamkennym@outlook.com>
2026-03-27 14:13:18 +0800

    Update app.py to allow external access by setting host to '0.0.0.0'

--- MPUFYP/app.py
+++ MPUFYP/app.py
@@ -11,4 +11,4 @@ def static_files(path):
     return send_from_directory('.', path)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=2526)
+    app.run(debug=True, host='0.0.0.0', port=2526)
